3677bd04f57264a1266213d134239369fed041e3
Breno Angelotti <brenogangelotti@gmail.com>
2022-02-22 03:56:40 -0300

    initial data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import { LinkButton } from './components/LinkButton';
+import Box from '@mui/material/Box';
+import { Paper } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <LinkButton title="Ver no Google Maps" url="https://goo.gl/maps/VpExroRC5gtXfGh39"/>
-        <LinkButton title="Ver no Waze" url="https://waze.com/ul/h6gggzugpy"/>
-      </header>
+    <div 
+      className="App"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL+'/img/pattern.jpg'})`,
+        backgroundRepeat: 'repeat'
+      }}
+    >
+        <Paper className='main-paper'>
+          <h3>Celebração de casamento</h3>
+          <h1>Vívian & Breno</h1>
+          <p>
+            Data: 14 de maio de 2022<br/>
+            Local: Chácara Recanto das Paineiras<br/>
+            Cornélio Procópio, PR
+          </p>
+          <Box sx={{ '& > :not(style)': { m: 1 } }}>
+            <LinkButton title="Ver no Maps" url="https://goo.gl/maps/VpExroRC5gtXfGh39"/>
+            <LinkButton title="Ver no Waze" url="https://waze.com/ul/h6gggzugpy"/>
+          </Box>
+        </Paper>
     </div>
   );
 }
--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { Fab } from '@mui/material';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 export class LinkButton extends Component{
     render(){
         return(
-            <a href={this.props.url}>{this.props.title}</a>
+            <Fab 
+                color="primary"
+                variant="extended" 
+                size="large"
+                onClick={() => window.open(this.props.url, '_blank').focus()}>
+                    <NavigationIcon sx={{ mr: 1 }}/>
+                    {this.props.title}
+            </Fab>
         )
     }
 }
