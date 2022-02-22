b940496b5cb0451a0c8c86e37b6480d7d97cfcbc
Breno Angelotti <brenogangelotti@gmail.com>
2022-02-22 02:27:24 -0300

    basic data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { LinkButton } from './components/LinkButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LinkButton title="Ver no Google Maps" url="https://goo.gl/maps/VpExroRC5gtXfGh39"/>
+        <LinkButton title="Ver no Waze" url="https://waze.com/ul/h6gggzugpy"/>
       </header>
     </div>
   );
